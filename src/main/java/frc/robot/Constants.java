6245779894cd80c7eadfdd5e17fc638622d84b30
Chris Knoebel <chrisgaming66@gmail.com>
2024-01-16 00:08:17 -0600

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,35 @@
+package frc.robot;
+
+public class Constants {
+    //public static final String robot = "SIM";
+    public static final String robot = "Real";
+
+    public static class SwerveConstants {
+        public static class Modules {
+            public static class FrontLeft {
+                public static int drive = 1;
+                public static int turn = 2;
+                public static int encoder = 9;
+                public static double encoderOffset = 0.0;
+            }
+            public static class FrontRight {
+                public static int drive = 3;
+                public static int turn = 4;
+                public static int encoder = 10;
+                public static double encoderOffset = 0.0;
+            }
+            public static class BackLeft {
+                public static int drive = 5;
+                public static int turn = 6;
+                public static int encoder = 11;
+                public static double encoderOffset = 0.0;
+            }
+            public static class BackRight {
+                public static int drive = 7;
+                public static int turn = 8;
+                public static int encoder = 12;
+                public static double encoderOffset = 0.0;
+            }
+        }
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.DriveWithController;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOADXRS450;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOSparkMax;
+
+public class RobotContainer {
+  // Subsystems
+  private Drive drive;
+
+  // Controller
+  private final CommandXboxController xboxOne = new CommandXboxController(0);
+  
+  
+  public RobotContainer() {
+    switch (Constants.robot) {
+      case "SIM":
+        drive = 
+          new Drive(
+            new GyroIO() {},
+            new ModuleIOSim(),
+            new ModuleIOSim(),
+            new ModuleIOSim(),
+            new ModuleIOSim());
+        break;
+      case "Real":
+        drive =
+          new Drive(
+            new GyroIOADXRS450() {},
+            new ModuleIOSparkMax(0),
+            new ModuleIOSparkMax(1),
+            new ModuleIOSparkMax(2),
+            new ModuleIOSparkMax(3));
+        break;
+    }
+    
+    // Create missing drive subsystem if needed.
+    if (drive == null) {
+      drive =
+        new Drive(
+          new GyroIO() {},
+          new ModuleIOSim(),
+          new ModuleIOSim(),
+          new ModuleIOSim(),
+          new ModuleIOSim());
+    }
+
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    // Joystick command factories
+    // Function<Boolean, DriveWithController> driveWithControllerFactory =
+    //     () ->
+    //       new DriveWithController(
+    //         drive,
+    //         () -> xboxOne.getLeftX(),
+    //         () -> xboxOne.getLeftY(),
+    //         () -> xboxOne.getRightX(),
+    //         () -> xboxOne.getRightY(),
+    //         () -> xboxOne.a().getAsBoolean()
+    //         );
+    drive.setDefaultCommand(
+      new DriveWithController(drive, () -> xboxOne.getLeftX(), () -> xboxOne.getLeftY(), () -> xboxOne.getRightX(), () -> xboxOne.getRightY(), () -> xboxOne.a().getAsBoolean())
+    );
+    xboxOne.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+    System.out.println("Set default command");
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
