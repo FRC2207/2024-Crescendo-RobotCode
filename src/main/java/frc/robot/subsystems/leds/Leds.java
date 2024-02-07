dc695202405bd1f9ac234a97197fe83604b35797
MrMusic64 <ethan08anderson@gmail.com>
2024-02-07 07:40:01 -0600

    removed unused imports

--- src/main/java/frc/robot/subsystems/leds/Leds.java
+++ src/main/java/frc/robot/subsystems/leds/Leds.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.leds;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.leds.LedsIO;
 import frc.robot.subsystems.leds.LedsIO.Section;
 
 public class Leds extends SubsystemBase {
