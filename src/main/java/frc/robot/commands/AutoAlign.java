5e041a254ad7f6509b8f4997baf295b6fa63d246
Chris Knoebel <chrisgaming66@gmail.com>
2024-02-23 23:09:18 -0600

    Fix offset autoalign poses. Fix obstacle avoidance

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -9,6 +9,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.FieldConstants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.util.AllianceFlipUtil;
@@ -34,23 +36,24 @@ public class AutoAlign extends DriveToPose {
                 if (target == Target.CENTER) {
                     nearestTarget = speakerCenterPose;
                 } else if (target == Target.SOURCESIDE) {
-                    nearestTarget = speakerCenterPose.plus(new Transform2d(.387, 1.085, new Rotation2d(Units.degreesToRadians(-50))));
+                    nearestTarget = speakerCenterPose.transformBy(new Transform2d(0.229, 1.095, new Rotation2d(Units.degreesToRadians(-40))));
                 } else if (target == Target.AMPSIDE) {
-                    nearestTarget = speakerCenterPose.plus(new Transform2d(.387, -1.085, new Rotation2d(Units.degreesToRadians(50))));;
+                    nearestTarget = speakerCenterPose.transformBy(new Transform2d(0.229, -1.095, new Rotation2d(Units.degreesToRadians(40))));;
                 } else {
                     nearestTarget = speakerCenterPose;
                 }
 
                 // Dont slam into stage
                 Pose2d allianceFlippedDrive = AllianceFlipUtil.apply(drive.getPose());
-                if (drive.getPose().getX() > FieldConstants.podiumX || drive.getPose().getX() < AllianceFlipUtil.apply(FieldConstants.podiumX)) {
+                if ((drive.getPose().getX() > FieldConstants.podiumX && DriverStation.getAlliance().get() == Alliance.Blue) || (drive.getPose().getX() < AllianceFlipUtil.apply(FieldConstants.podiumX) && DriverStation.getAlliance().get() == Alliance.Red)) {
                     nearestTarget = new Pose2d(nearestTarget.getX(), MathUtil.clamp(nearestTarget.getY(), allianceFlippedDrive.getY(), 4 - 2 * (allianceFlippedDrive.getX()-FieldConstants.podiumX)), nearestTarget.getRotation());
                 }
 
                 // Dont slam into subwoofer
-                if (drive.getPose().getY() < FieldConstants.Speaker.topLeftSpeaker.getY() - 0.5 || drive.getPose().getY() > FieldConstants.Speaker.topRightSpeaker.getY() + 0.5) {
-                    nearestTarget = new Pose2d(MathUtil.clamp(nearestTarget.getX(), 1.5, Double.MAX_VALUE), nearestTarget.getY(), nearestTarget.getRotation());
-                }
+                //if ((target == Target.AMPSIDE || target == Target.SOURCESIDE) && (drive.getPose().getY() < FieldConstants.Speaker.topLeftSpeaker.getY() - 0.25 || drive.getPose().getY() > FieldConstants.Speaker.topRightSpeaker.getY() + 0.25)) {
+                //    nearestTarget = new Pose2d(MathUtil.clamp(nearestTarget.getX(), 1.5, Double.MAX_VALUE), nearestTarget.getY(), nearestTarget.getRotation());
+                //}
+                
                 
                 nearestTarget = AllianceFlipUtil.apply(nearestTarget);
 
