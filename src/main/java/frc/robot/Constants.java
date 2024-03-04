938893eb09f8c95ef3b0423e3e4c9a1abf6e6620
MrMusic64 <ethan08anderson@gmail.com>
2024-03-03 20:06:13 -0600

    Fixed angle limits

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,8 +46,8 @@ public class Constants {
         public static int pivotMotorID = 15;
         public static int pivotEncoderID = 1;
         public static int pivotGearRatio = 80;
-        public static double pivotMaxAngleRad = .25 * (2 * Math.PI);
-        public static double pivotMinAngleRad = 0 * (2 * Math.PI);
+        public static double pivotMaxAngleRad = 2.6;  //2.6 is absolute max
+        public static double pivotMinAngleRad = 0.25;  // .25 is absolute min
         public static double rawPivotSpeedLimiter = 0.25;
 
         public static int kP = 1;
